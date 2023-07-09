e7f09e20c412736bbbd2f32b6f59fc0a20ec8544
0xMatiz <matias.e.zabala@gmail.com>
2023-07-09 04:09:52 -0300

    update

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ function validateEmail(email) {
   // Puedes usar expresiones regulares u otras técnicas de validación
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
+
+// Agregar lógica de desplazamiento suave a los enlaces de navegación
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
